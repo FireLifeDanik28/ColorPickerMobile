ed17a583095f43397ed6850a6d53268f9f8bafd7
4191849 <4191849@TE-GDA-D-D37H02>
2025-02-03 11:04:43 +0100

    Dodaj pliki projektów.

---
+++ ColorPickerMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ColorPickerMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Picker();
+        }
+    }
+}
---
+++ ColorPickerMobile/Picker.xaml.cs
@@ -0,0 +1,9 @@
+namespace ColorPickerMobile;
+
+public partial class Picker : ContentPage
+{
+	public Picker()
+	{
+		InitializeComponent();
+	}
+}
